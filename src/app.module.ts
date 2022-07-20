99a0422f363931359c534b62fc3e5ad92f5c79fc
Raihan Robbani <raihan.robbani@mcpayment.co.id>
2022-07-21 00:31:41 +0700

    add connection with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { HelperModule } from './helper/helper.module';
+import { JwtModule } from './jwt/jwt.module';
+import { BcryptModule } from './bcrypt/bcrypt.module';
 import 'reflect-metadata';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/User';
+import { Balance } from './entity/Balance';
+import { Transaction } from './entity/Transaction';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User, Balance, Transaction],
+      synchronize: true,
+    }),
+    UserModule,
+    HelperModule,
+    JwtModule,
+    BcryptModule,
   ],
   controllers: [AppController],
   providers: [AppService],
