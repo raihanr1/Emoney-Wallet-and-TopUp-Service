c4d6e9192cf56f3f333a602d541a98937789f346
Raihan Robbani <raihan.robbani@mcpayment.co.id>
2022-07-20 19:02:05 +0700

    add config module for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'reflect-metadata';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
